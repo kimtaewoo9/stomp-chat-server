f8be9f129266d2dca1ea948aa34cd10d9e6469c5
kitmtaewoo <bezzi1654@kookmin.ac.kr>
2025-05-02 19:14:28 +0900

    feat: MemberSaveReqDto 생성

---
+++ chatserver/src/main/java/com/example/chatserver/dto/MemberSaveReqDto.java
@@ -0,0 +1,19 @@
+package com.example.chatserver.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+public class MemberSaveReqDto {
+
+    private String name;
+    private String email;
+    private String password;
+}
