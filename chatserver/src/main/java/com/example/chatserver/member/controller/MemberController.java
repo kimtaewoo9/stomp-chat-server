53e57b4db3f4d6b18a9fb8d8ad7790842c351cd5
kitmtaewoo <bezzi1654@kookmin.ac.kr>
2025-05-03 20:49:37 +0900

    feat: 컨트롤러 구현

--- chatserver/src/main/java/com/example/chatserver/member/controller/MemberController.java
+++ chatserver/src/main/java/com/example/chatserver/member/controller/MemberController.java
@@ -1,11 +1,10 @@
-package com.example.chatserver.controller;
+package com.example.chatserver.member.controller;
 
-import com.example.chatserver.domain.Member;
-import com.example.chatserver.dto.MemberSaveReqDto;
-import com.example.chatserver.service.MemberService;
+import com.example.chatserver.member.domain.Member;
+import com.example.chatserver.member.dto.MemberSaveReqDto;
+import com.example.chatserver.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +16,7 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping("/member/create")
-    public ResponseEntity<?> memberCreate(@RequestBody MemberSaveReqDto requestDto){
+    public ResponseEntity<?> memberCreate(@RequestBody MemberSaveReqDto requestDto) {
 
         Member member = memberService.create(requestDto);
         return ResponseEntity.ok(member.getId());
