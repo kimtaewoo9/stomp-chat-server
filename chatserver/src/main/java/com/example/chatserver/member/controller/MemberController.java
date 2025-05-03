8fb07c4a2b0c32de7199030b91af17f0ed0cedf3
kimtaewoo9 <tplaytive@gmail.com>
2025-05-03 23:54:40 +0900

    feat: doLogin 기능 구현

--- chatserver/src/main/java/com/example/chatserver/member/controller/MemberController.java
+++ chatserver/src/main/java/com/example/chatserver/member/controller/MemberController.java
@@ -1,8 +1,13 @@
 package com.example.chatserver.member.controller;
 
+import com.example.chatserver.common.auth.JwtTokenProvider;
 import com.example.chatserver.member.domain.Member;
+import com.example.chatserver.member.domain.Role;
+import com.example.chatserver.member.dto.MemberLoginReqDto;
 import com.example.chatserver.member.dto.MemberSaveReqDto;
 import com.example.chatserver.member.service.MemberService;
+import java.util.HashMap;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,10 +20,25 @@ public class MemberController {
 
     private final MemberService memberService;
 
+    private final JwtTokenProvider jwtTokenprovider;
+
     @PostMapping("/member/create")
     public ResponseEntity<?> memberCreate(@RequestBody MemberSaveReqDto requestDto) {
 
         Member member = memberService.create(requestDto);
         return ResponseEntity.ok(member.getId());
+    }
+
+    @PostMapping("/member/doLogin")
+    public ResponseEntity<?> doLogin(@RequestBody MemberLoginReqDto memberLoginReqDto){
+        Member member = memberService.doLogin(memberLoginReqDto);
+        
+        // 일치할 경우 access 토큰 발행 후 사용자에게 돌려준다.
+        String jwtToken = jwtTokenprovider.createToken(member.getEmail(), Role.USER); // 일단 다 User
+        Map<String, Object> loginInfo = new HashMap<>();
+        loginInfo.put("id", member.getId());
+        loginInfo.put("token", jwtToken);
+
+        return ResponseEntity.ok(loginInfo);
     }
 }
--- chatserver/src/main/java/com/example/chatserver/member/service/MemberService.java
+++ chatserver/src/main/java/com/example/chatserver/member/service/MemberService.java
@@ -1,9 +1,13 @@
 package com.example.chatserver.member.service;
 
+import com.example.chatserver.common.configs.SecurityConfigs;
 import com.example.chatserver.member.domain.Member;
+import com.example.chatserver.member.dto.MemberLoginReqDto;
 import com.example.chatserver.member.dto.MemberSaveReqDto;
 import com.example.chatserver.member.repository.MemberRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,8 +19,9 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
 
-    public Member create(@RequestBody MemberSaveReqDto requestDto) {
+    private final PasswordEncoder passwordEncoder;
 
+    public Member create(@RequestBody MemberSaveReqDto requestDto) {
         String name = requestDto.getName();
         String email = requestDto.getEmail();
         String password = requestDto.getPassword();
@@ -28,10 +33,23 @@ public class MemberService {
         Member newMember = Member.builder()
             .name(name)
             .email(email)
-            .password(password)
+            .password(passwordEncoder.encode(password))
             .build();
         Member savedMember = memberRepository.save(newMember);
 
         return savedMember;
+    }
+
+    public Member doLogin(MemberLoginReqDto loginDto){
+        String loginEmail = loginDto.getEmail();
+        String loginPassword = loginDto.getPassword();
+
+        Member member = memberRepository.findByEmail(loginEmail).orElseThrow(
+            () -> new EntityNotFoundException("존재하지 않는 이메일 입니다."));
+        if(!passwordEncoder.matches(member.getPassword(), loginPassword)){
+            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+        }
+
+        return member;
     }
 }
