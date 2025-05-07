a20782e98e4578b96c922cfc281623383ebfe66d
kitmtaewoo <bezzi1654@kookmin.ac.kr>
2025-05-08 00:11:11 +0900

    refactor: railway에 배포된 도메인만 접근할 수 있도록 변경

--- chat-front/vue.config.js
+++ chat-front/vue.config.js
@@ -2,6 +2,8 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    allowedHosts: 'all'
+    allowedHosts: [
+      'stomp-chat-server.up.railway.app'
+    ]
   }
 })
