056a4ebf545f31c0017546432a2fe0c31f7b4f7c
kitmtaewoo <bezzi1654@kookmin.ac.kr>
2025-05-07 18:21:50 +0900

    feat: 회원 목록 기능 구현

--- chatserver/src/main/java/com/example/chatserver/member/controller/MemberController.java
+++ chatserver/src/main/java/com/example/chatserver/member/controller/MemberController.java
@@ -4,12 +4,15 @@ import com.example.chatserver.common.auth.JwtTokenProvider;
 import com.example.chatserver.member.domain.Member;
 import com.example.chatserver.member.domain.Role;
 import com.example.chatserver.member.dto.MemberLoginReqDto;
+import com.example.chatserver.member.dto.MemberResponseDto;
 import com.example.chatserver.member.dto.MemberSaveReqDto;
 import com.example.chatserver.member.service.MemberService;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,9 +33,9 @@ public class MemberController {
     }
 
     @PostMapping("/member/doLogin")
-    public ResponseEntity<?> doLogin(@RequestBody MemberLoginReqDto memberLoginReqDto){
+    public ResponseEntity<?> doLogin(@RequestBody MemberLoginReqDto memberLoginReqDto) {
         Member member = memberService.doLogin(memberLoginReqDto);
-        
+
         // 일치할 경우 access 토큰 발행 후 사용자에게 돌려준다.
         String jwtToken = jwtTokenprovider.createToken(member.getEmail(), Role.USER); // 일단 다 User
         Map<String, Object> loginInfo = new HashMap<>();
@@ -40,5 +43,11 @@ public class MemberController {
         loginInfo.put("token", jwtToken);
 
         return ResponseEntity.ok(loginInfo);
+    }
+
+    @GetMapping("/member/list")
+    public ResponseEntity<List<MemberResponseDto>> memberList() {
+        List<MemberResponseDto> memberResponseDtos = memberService.getMemberList();
+        return ResponseEntity.ok(memberResponseDtos);
     }
 }
