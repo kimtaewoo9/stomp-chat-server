3958b568e4542b5628760689a054c9eab8734c09
kitmtaewoo <bezzi1654@kookmin.ac.kr>
2025-05-07 18:22:13 +0900

    refactor: 나의 채팅 목록 기능 수정

--- chatserver/src/main/java/com/example/chatserver/chat/controller/ChatController.java
+++ chatserver/src/main/java/com/example/chatserver/chat/controller/ChatController.java
@@ -21,29 +21,29 @@ public class ChatController {
     private final ChatService chatService;
 
     @PostMapping("/chat/room/group/create")
-    public ResponseEntity<?> createGroupChatRoom(@RequestParam String roomName){
+    public ResponseEntity<?> createGroupChatRoom(@RequestParam String roomName) {
         chatService.createChatRoom(roomName);
 
         return ResponseEntity.ok().build();
     }
 
     @GetMapping("/chat/room/group/list")
-    public ResponseEntity<?> getGroupChatRooms(){
+    public ResponseEntity<?> getGroupChatRooms() {
         List<ChatRoomResponseDto> chatRooms = chatService.getGroupChatRooms();
 
         return ResponseEntity.ok(chatRooms);
     }
 
     @PostMapping("/chat/room/group/{roomId}/join")
-    public ResponseEntity<?> joinGroupChat(@PathVariable Long id){
-        chatService.addParticipantToGroupChat(id);
+    public ResponseEntity<?> joinGroupChat(@PathVariable Long roomId) {
+        chatService.addParticipantToGroupChat(roomId);
 
         return ResponseEntity.ok().build();
     }
 
     @GetMapping("/chat/history/{roomId}")
-    public ResponseEntity<?> getChatHistory(@PathVariable Long id){
-        List<ChatMessageDto> chatMessageDto = chatService.getChatHistory(id);
+    public ResponseEntity<?> getChatHistory(@PathVariable Long roomId) {
+        List<ChatMessageDto> chatMessageDto = chatService.getChatHistory(roomId);
 
         return ResponseEntity.ok(chatMessageDto);
     }
@@ -51,29 +51,29 @@ public class ChatController {
     // 메시지 읽음 처리 .. -> disconnect 시점에 채팅방에 있는 메시지들 전부 읽음 처리 .
     // 채팅룸 ID를 주면서 "나 그 채팅방에 있는 메시지 전부 읽었어" 라고 전달
     @PostMapping("/chat/room/{roomId}/read")
-    public ResponseEntity<?> readAllMessages(@PathVariable Long id){
-        chatService.readAllMessages(id);
+    public ResponseEntity<?> readAllMessages(@PathVariable Long roomId) {
+        chatService.readAllMessages(roomId);
 
         return ResponseEntity.ok().build();
     }
 
     // 내 채팅방 목록 조회 dto roomId, roomName, 읽지 않은 메시지 개수, group 채팅 여부 .
     @GetMapping("/chat/my/rooms")
-    public ResponseEntity<?> getMyChatRooms(){
+    public ResponseEntity<?> getMyChatRooms() {
 
         List<MyChatRoomResponseDto> myChatRoomListResponseDtos = chatService.getMyChatRooms();
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(myChatRoomListResponseDtos);
     }
 
     @DeleteMapping("/chat/room/{roomId}/leave")
-    public ResponseEntity<?> leaveGroupChatroom(@PathVariable Long roomId){
+    public ResponseEntity<?> leaveGroupChatroom(@PathVariable Long roomId) {
         chatService.leaveGroupChatRoom(roomId);
 
         return ResponseEntity.ok().build();
     }
 
     @PostMapping("/room/private/create")
-    public ResponseEntity<?> getOrCreatePrivateRoom(@RequestParam Long otherMemberId){
+    public ResponseEntity<?> getOrCreatePrivateRoom(@RequestParam Long otherMemberId) {
         Long roomId = chatService.getOrCreatePrivateRoom(otherMemberId);
         return ResponseEntity.ok(roomId);
     }
