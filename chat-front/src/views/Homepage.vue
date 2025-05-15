b9d7144a82bed6faeaf543b5a02fd00c8fd20a57
kitmtaewoo <bezzi1654@kookmin.ac.kr>
2025-05-15 11:38:28 +0900

    feat: 파비콘 수정, server 이름 stompgram으로 수정

--- chat-front/src/views/Homepage.vue
+++ chat-front/src/views/Homepage.vue
@@ -1,9 +1,8 @@
-<!-- HomePage.vue -->
 <template>
   <div class="home-container">
     <div class="intro-section">
       <h1>
-        STOMP CHAT <sup class="version">V1</sup>
+        STOMPGRAM <sup class="version">V1</sup>
       </h1>
       <p class="tagline">A real-time communication platform</p>
       
